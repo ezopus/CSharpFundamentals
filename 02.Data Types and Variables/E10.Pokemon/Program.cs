988d0e1124391f0d1015b15827d64363685e2242
ezopus <133430790+ezopus@users.noreply.github.com>
2023-05-19 22:52:39 +0300

    complete set

--- 02.Data Types and Variables/E10.Pokemon/Program.cs
+++ 02.Data Types and Variables/E10.Pokemon/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿decimal N = decimal.Parse(Console.ReadLine());
+decimal M = decimal.Parse(Console.ReadLine());
+int Y = int.Parse(Console.ReadLine());
+int pokeCounter = 0;
+decimal originalValue = N;
+while (N >= M)
+{
+    N -= M;
+    pokeCounter++;
+    if (originalValue / 2 == N && Y > 0 && Y < 10)
+    {
+        N = (int)N/Y;
+    }
+        
+}
+Console.WriteLine(N);
+Console.WriteLine(pokeCounter);
+
---
+++ 02.Data Types and Variables/E11.Snowballs/Program.cs
@@ -0,0 +1,21 @@
+﻿int totalSnowballs = int.Parse(Console.ReadLine());
+int bestSnowballSnow = 0;
+int bestSnowballTime = 0;
+int bestSnowballQuality = 0;
+decimal bestValue = 0;
+for (int i = 0; i < totalSnowballs; i++)
+{
+    int snowballSnow = int.Parse(Console.ReadLine());
+    int snowballTime = int.Parse(Console.ReadLine());
+    int snowballQuality = int.Parse(Console.ReadLine());
+    decimal snowballValue = (decimal)Math.Pow((snowballSnow / snowballTime), snowballQuality);
+    if (snowballValue > bestValue)
+    {
+        bestSnowballSnow = snowballSnow;
+        bestSnowballTime = snowballTime;
+        bestSnowballQuality = snowballQuality;
+        bestValue = snowballValue;
+    }
+}
+Console.WriteLine($"{bestSnowballSnow} : {bestSnowballTime} = {bestValue} ({bestSnowballQuality})");
+
