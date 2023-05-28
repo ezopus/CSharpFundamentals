a1adf5f2bdc7963349f279f5cff79564d138dd14
ezopus <133430790+ezopus@users.noreply.github.com>
2023-05-28 23:52:35 +0300

    more solutions added

---
+++ 03.Arrays/M01.EncryptSortPrintArray/Program.cs
@@ -0,0 +1,35 @@
+﻿int inputCount = int.Parse(Console.ReadLine());
+int[] allInputs = new int[inputCount];
+for (int i = 0; i < inputCount; i++)
+{
+    char[] totalChars = Console.ReadLine().ToCharArray();
+    int sumOfChars = 0;
+    for (int j = 0; j < totalChars.Length; j++)
+    {
+        char oneChar = totalChars[j];
+        switch (oneChar)
+        {
+            case 'A':
+            case 'O':
+            case 'E':
+            case 'I':
+            case 'U':
+            case 'a':
+            case 'o':
+            case 'e':
+            case 'i':
+            case 'u':
+                sumOfChars += (int)oneChar * totalChars.Length;
+                break;
+            default:
+                sumOfChars += (int)oneChar / totalChars.Length;
+                break;
+        }
+    }
+    allInputs[i] = sumOfChars;
+}
+Array.Sort(allInputs);
+foreach (int number in allInputs)
+{
+    Console.WriteLine(number);
+}
---
+++ 03.Arrays/M02.PascalTriangle/Program.cs
@@ -0,0 +1,11 @@
+﻿int input = int.Parse(Console.ReadLine());
+for (int i = 0; i < input; i++)
+{
+    int[] arrayOne = new int[i+1];
+    for (int j = 0; j < i; j++)
+    {
+        arrayOne[i] = i + j;
+    }
+    Console.Write(string.Join(" ", arrayOne));
+    Console.WriteLine();
+}
