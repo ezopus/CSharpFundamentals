1b45d9e6869e6449254aab81a9b518aa476fcddb
ezopus <133430790+ezopus@users.noreply.github.com>
2023-06-07 00:20:51 +0300

    pascal problem solution

--- 03.Arrays/M02.PascalTriangle/Program.cs
+++ 03.Arrays/M02.PascalTriangle/Program.cs
@@ -1,11 +1,36 @@
-﻿int input = int.Parse(Console.ReadLine());
-for (int i = 0; i < input; i++)
+﻿using System.Diagnostics.Metrics;
+
+int input = int.Parse(Console.ReadLine());
+long[] tempArray;
+Console.WriteLine(1);
+long[] row = { 1 };
+while (input > 1)
 {
-    int[] arrayOne = new int[i+1];
-    for (int j = 0; j < i; j++)
+    int counter = row.Length;
+    long[] nextRow = new long[counter + 1];
+    nextRow[0] = 1;
+    nextRow[nextRow.Length - 1] = 1;
+    for (int i = 1; i < row.Length; i++)
     {
-        arrayOne[i] = i + j;
+        nextRow[i] = row[i] + row[i - 1];
     }
-    Console.Write(string.Join(" ", arrayOne));
-    Console.WriteLine();
+    row = new long[nextRow.Length];
+    row = nextRow;
+    Console.WriteLine(string.Join(' ', nextRow));
+    input--;
 }
+//
+//
+//
+// int input = int.Parse(Console.ReadLine());
+//
+// for (long line = 1; line <= input; line++)
+// {
+//     long C = 1;
+//     for (long i = 1; i <= line; i++)
+//     {
+//         Console.Write(C + " ");
+//         C = C * (line - i) / i;
+//     }
+//     Console.WriteLine();
+// }
