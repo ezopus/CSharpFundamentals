133c3b3b7aff258a9580e4e01c919b34d3a3acd8
ezopus <133430790+ezopus@users.noreply.github.com>
2023-06-08 00:33:07 +0300

    first two exercises done

---
+++ 05.Lists/E01Train/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E01.Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> passengers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            int wagonCapacity = int.Parse(Console.ReadLine());
+
+            string input = "";
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] command = input.Split();
+                if (command[0] == "Add")
+                {
+                    passengers.Add(int.Parse(command[1]));
+                }
+                else
+                {
+                    int newPassengers = int.Parse(command[0]);
+                    for (int i = 0; i < passengers.Count; i++)
+                    {
+                        if (newPassengers + passengers[i] <= wagonCapacity)
+                        {
+                            passengers[i] += newPassengers;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", passengers));
+        }
+    }
+}
---
+++ 05.Lists/E02.ChangeList/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E02.ChangeList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numberString = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToList();
+            string input = "";
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] command = input.Split();
+                switch (command[0])
+                {
+                    case "Delete":
+                        int deleteElement = int.Parse(command[1]);
+                        DeleteElement(numberString, deleteElement);
+                        break;
+                    case "Insert":
+                        int element = int.Parse(command[1]);
+                        int position = int.Parse(command[2]);
+                        InsertElement(numberString, element, position);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numberString));
+        }
+
+        private static void InsertElement(List<int> numberString, int element, int position)
+        {
+            numberString.Insert(position,element);
+        }
+
+        private static void DeleteElement(List<int> numberString,int deleteElement)
+        {
+            for (int i = 0; i < numberString.Count; i++)
+            {
+                if (numberString[i] == deleteElement)
+                {
+                    numberString.RemoveAt(i);
+                }
+            }
+        }
+    }
+}
