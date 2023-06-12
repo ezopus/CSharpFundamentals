6bda2f74cd493443dc3900339b0f82c0d0c7190d
ezopus <133430790+ezopus@users.noreply.github.com>
2023-06-13 01:33:58 +0300

    final exercise from lists -> more exercises

---
+++ 05.Lists/M05.DrumSet/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace M05.DrumSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double savings = double.Parse(Console.ReadLine());
+            List<int> drumset = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+            List<int> initialQuality = drumset.ToList();
+            string input = "";
+            int counterRemovedDrums = 0;
+            while ((input = Console.ReadLine()) != "Hit it again, Gabsy!")
+            {
+                int hitPower = int.Parse(input);
+                for (int i = 0; i < drumset.Count; i++)
+                {
+                    drumset[i] -= hitPower;
+                    if (drumset[i] <= 0)
+                    {
+                        int replacementDrum = initialQuality[i + counterRemovedDrums] * 3;
+                        if (savings - replacementDrum >= 0)
+                        {
+                            savings -= initialQuality[i + counterRemovedDrums] * 3;
+                            drumset[i] = initialQuality[i + counterRemovedDrums];
+                        }
+                        else
+                        {
+                            drumset.RemoveAt(i);
+                            i--;
+                            if (counterRemovedDrums < i)
+                            {
+                                counterRemovedDrums++;
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', drumset));
+            Console.WriteLine($"Gabsy has {savings:f2}lv.");
+        }
+    }
+}
