63801f945b0320f50237f35e1422c3405e627b65
ezopus <133430790+ezopus@users.noreply.github.com>
2023-06-28 23:43:59 +0300

    clarifying comments added

--- 07.AssociativeArrays/M01.Ranking/Program.cs
+++ 07.AssociativeArrays/M01.Ranking/Program.cs
@@ -9,7 +9,6 @@ while ((input = Console.ReadLine()) != "end of contests")
 }
 
 var users = new Dictionary<string, List<ContestResults>>();
-
 while ((input = Console.ReadLine()) != "end of submissions")
 {
     string[] tokens = input.Split("=>");
@@ -17,27 +16,27 @@ while ((input = Console.ReadLine()) != "end of submissions")
     string password = tokens[1];
     string username = tokens[2];
     double points = double.Parse(tokens[3]);
-    if (contests.ContainsKey(contestName))
+    if (contests.ContainsKey(contestName))    //check if user contest exists in legit contests
     {
-        if (password == contests[contestName])
+        if (password == contests[contestName])   //check if user password matches existing chosen contest password
         {
             ContestResults currentContestResult = new ContestResults(contestName, points);
-            if (!users.ContainsKey(username))
+            if (!users.ContainsKey(username))    //if user participates for first time in chosen contest, add username, contest name and result
             {
                 users.Add(username, new List<ContestResults>());
                 users[username].Add(currentContestResult);
             }
-            else
+            else   //if user already participated in contest and score needs updating
             {
-                ContestResults existingResults = users[username].Find(x => x.Contest == contestName);
-                if (existingResults != null)
+                ContestResults existingResults = users[username].Find(x => x.Contest == contestName);   //try and get contest from dictionary, where user has previously participated in
+                if (existingResults != null)   //if previous contest exists
                 {
-                    if (existingResults.Points < points)
+                    if (existingResults.Points < points)  //check if score is lower and update if yes
                     {
                         existingResults.Points = points;
                     }
                 }
-                else users[username].Add(currentContestResult);
+                else users[username].Add(currentContestResult);   //add first user score to username for current contest
             }
         }
     }
