41f344b73f36ec3b3a2e88f4d46e630913581be5
ezopus <133430790+ezopus@users.noreply.github.com>
2023-07-01 22:02:00 +0300

    snowwhite and moba exercise solutions added

---
+++ 07.AssociativeArrays/M03.MobaChallenger/Program.cs
@@ -0,0 +1,76 @@
+﻿var players = new Dictionary<string, List<PositionSkill>>();
+string input = "";
+while ((input = Console.ReadLine()) != "Season end")
+{
+    string[] tokens = input.Replace(" -> ", " ").Split(" ");
+    string playerOne = tokens[0];
+    string PositionOrDuel = tokens[1];
+    if (PositionOrDuel == "vs")
+    {
+        string playerTwo = tokens[2];
+        if (players.ContainsKey(playerOne) && players.ContainsKey(playerTwo))
+        {
+            bool HasMatchingPositions = players[playerOne].Exists(x => players[playerTwo].Exists(y => x.Position == y.Position));
+            int totalSkillPlayerOne = players[playerOne].Sum(x => x.Skill);
+            int totalSkillPlayerTwo = players[playerTwo].Sum(x => x.Skill);
+            if (HasMatchingPositions)
+            {
+                if (totalSkillPlayerOne > totalSkillPlayerTwo)
+                {
+                    players.Remove(playerTwo);
+                }
+                if (totalSkillPlayerOne < totalSkillPlayerTwo)
+                {
+                    players.Remove(playerOne);
+                }
+            }
+        }
+    }
+    else
+    {
+        int skill = int.Parse(tokens[2]);
+        if (players.ContainsKey(playerOne))
+        {
+            PositionSkill current = players[playerOne].Find(x => x.Position == PositionOrDuel);
+            if (current != null)
+            {
+                if (current.Skill < skill)
+                {
+                    current.Skill = skill;
+                }
+            }
+            else
+            {
+                PositionSkill newSkill = new PositionSkill(playerOne, PositionOrDuel, skill);
+                players[playerOne].Add(newSkill);
+            }
+        }
+        else
+        {
+            players.Add(playerOne, new List<PositionSkill>());
+            PositionSkill newSkill = new PositionSkill(playerOne,PositionOrDuel, skill);
+            players[playerOne].Add(newSkill);
+        }
+    }
+}
+
+foreach (var p in players.OrderByDescending(x => x.Value.Sum(y => y.Skill)).ThenBy(x => x.Key))
+{
+    Console.WriteLine($"{p.Key}: {p.Value.Sum(x => x.Skill)} skill");
+    foreach (var s in p.Value.OrderByDescending(x => x.Skill).ThenBy(x => x.Position))
+    {
+        Console.WriteLine($"- {s.Position} <::> {s.Skill}");
+    }
+}
+public class PositionSkill
+{
+    public PositionSkill(string name, string position, int skill)
+    {
+        Name = name;
+        Position = position;
+        Skill = skill;
+    }
+    public string Name { get; set; }
+    public string Position { get; set; }
+    public int Skill { get; set; }
+}
---
+++ 07.AssociativeArrays/M04.Snowwhite/Program.cs
@@ -0,0 +1,42 @@
+﻿string input = "";
+var dwarvesList = new List<Dwarf>();
+while ((input = Console.ReadLine()) != "Once upon a time")
+{
+    string[] tokens = input.Split(" <:> ");
+    string name = tokens[0];
+    string hatColor = tokens[1];
+    int physics = int.Parse(tokens[2]);
+    Dwarf current = new Dwarf(name, hatColor, physics);
+    Dwarf existingDwarf = dwarvesList.Find(x => x.Name == name && x.HatColor == hatColor);
+    if (existingDwarf != null)
+    {
+        if (existingDwarf.Physics < physics)
+        {
+            existingDwarf.Physics = physics;
+        }
+    }
+    else
+    {
+        dwarvesList.Add(current);
+    }
+}
+
+foreach (var dwarf in dwarvesList.OrderByDescending(x => x.Physics).ThenByDescending(x => dwarvesList.Count(y => y.HatColor == x.HatColor)))
+{
+    Console.WriteLine($"({dwarf.HatColor}) {dwarf.Name} <-> {dwarf.Physics}");
+}
+
+public class Dwarf
+{
+    public Dwarf(string name, string hatColor, int physics)
+    {
+        Name = name;
+        HatColor = hatColor;
+        Physics = physics;
+        //Physics = physics;
+    }
+
+    public string Name { get; set; }
+    public string HatColor { get; set; }
+    public int Physics { get; set; }
+}
