339927cec8fb3e5464acd73fecae2e6e63c97b2d
ezopus <133430790+ezopus@users.noreply.github.com>
2023-07-08 16:39:29 +0300

    Update Program.cs

--- 08.TextProcessing/M05.HTML/Program.cs
+++ 08.TextProcessing/M05.HTML/Program.cs
@@ -8,14 +8,14 @@ while ((comments = Console.ReadLine()) != "end of comments")
 }
 
 Console.WriteLine("<h1>");
-Console.WriteLine($"{title.PadLeft(5)}");
+Console.WriteLine($"\t{title.PadLeft(5)}");
 Console.WriteLine("</h1>");
 Console.WriteLine("<article>");
-Console.WriteLine($"{content.PadLeft(5)}");
+Console.WriteLine($"\t{content.PadLeft(5)}");
 Console.WriteLine("</article>");
 foreach (string comment in commentList)
 {
     Console.WriteLine("<div>");
-    Console.WriteLine($"{comment.PadLeft(5)}");
+    Console.WriteLine($"\t{comment.PadLeft(5)}");
     Console.WriteLine("</div>");
 }
